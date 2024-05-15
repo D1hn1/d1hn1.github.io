02dfdcd332752c9123551d136e86ac28c53b1377
d1hn1 <daniellopezsantiago27@gmail.com>
2024-05-15 13:58:25 +0200

    Updated!

--- script.js
+++
@@ -1,9 +0,0 @@
-let star = document.getElementById("star-icon");
-
-function scroll_variant () {
-	let scrollY = ( window.scrollY / 2 );
-	star.style.transform = 'rotate(' + scrollY + 'deg)';
-	console.log(scrollY);
-}
-
-setInterval(scroll_variant, 100);
